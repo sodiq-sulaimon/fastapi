600888b3387dd35f038bb54260857532e5c0ff8d
Sodiq Sulaimon <sodiq.sulaimon204@gmail.com>
2024-01-12 20:37:13 -0600

    Updated posts.py

--- posts.py
+++ posts.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from fastapi.params import Body
+# from fastapi.params import Body
 from pydantic import BaseModel
+from typing import Optional
 
 post_app = FastAPI()
 
@@ -9,6 +10,7 @@ class Post(BaseModel):
     title: str
     content: str
     draft: bool = False
+    rating: Optional[int] = None
 
 @post_app.get("/")
 def root():
@@ -17,8 +19,11 @@ def root():
 @post_app.post("/createpost")
 # def create_post(new_post: dict = Body(...)):
 def create_post(new_post: Post):
+    print(new_post.user_id, ":", new_post.title)
+    print("Rating: ", new_post.rating)
     if new_post.draft is True:
         return ("Post saved as draft.")
     else:
-        return new_post.content
+        new_post.dict
+        return {"Data": new_post}
 
