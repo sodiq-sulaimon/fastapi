e6e582114100b5e86c84f8bdb429ffad8e586c10
Sodiq Sulaimon <sodiq.sulaimon204@gmail.com>
2024-01-16 11:28:07 -0600

    Updated posts.py with additional practice

--- posts.py
+++ posts.py
@@ -2,28 +2,34 @@ from fastapi import FastAPI
 # from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
 
-post_app = FastAPI()
+app = FastAPI()
 
 class Post(BaseModel):
-    user_id : int
     title: str
     content: str
     draft: bool = False
     rating: Optional[int] = None
 
-@post_app.get("/")
+@app.get("/")
 def root():
     return {"Message: Hello, World!"}
 
-@post_app.post("/createpost")
+saved_posts = []
+
+@app.post("/posts")
 # def create_post(new_post: dict = Body(...)):
-def create_post(new_post: Post):
-    print(new_post.user_id, ":", new_post.title)
-    print("Rating: ", new_post.rating)
-    if new_post.draft is True:
+def create_post(post: Post):
+    post_dict = post.model_dump()
+    post_dict["id"] = randrange(1, 1000000)
+    saved_posts.append(post_dict)
+    if post.draft is True:
         return ("Post saved as draft.")
     else:
-        new_post.dict
-        return {"Data": new_post}
+        return post.content
 
+
+@app.get("/posts")
+def get_posts():
+    return saved_posts
